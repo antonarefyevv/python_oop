86bed825c9ba4ae5096ddf2f2efcd227785503a9
antonarefyevv <virt4444@mail.ru>
2024-03-30 22:23:25 +0700

    Initial commit

--- shape_task/main.py
+++ shape_task/main.py
@@ -4,44 +4,30 @@ from shape_task.square import Square
 from shape_task.circle import Circle
 
 
-def get_shape_with_max_area(items):
-    def sort_shape(shape):
-        return shape.get_area()
-
-    sorted_list = sorted(items, key=sort_shape, reverse=True)
-
+def get_shape_with_max_area(shapes):
+    if len(shapes) == 0:
+        return None
+    sorted_list = sorted(shapes, key=lambda sort_shape: sort_shape.get_area(), reverse=True)
     return sorted_list[0]
 
 
-def get_shape_with_next_perimeter_after_max(items):
-    def sort_shape(shape):
-        return shape.get_perimeter()
-
-    sorted_list = sorted(items, key=sort_shape, reverse=True)
+def get_shape_with_next_perimeter_after_max(shapes):
+    if len(shapes) == 0:
+        return None
+    sorted_list = sorted(shapes, key=lambda sort_shape: sort_shape.get_area(), reverse=True)
 
     return sorted_list[1]
 
 
-rectangle_1 = Rectangle(9, 5)
+shapes_list = [Square(4), Square(15), Triangle(0, 2, 1, 5, 1, 8), Triangle(0, 2, 1, 5, 1, 8), Rectangle(9, 5),
+               Circle(8)]
 
-circle_1 = Circle(10)
+shapes_list_2 = []
 
-triangle_1 = Triangle(1, 0, 5, 2, 6, 7)
-print(triangle_1)
-
-square_1 = Square(5)
-
-square_2 = Square(15)
-
-triangle_2 = Triangle(0, 2, 1, 5, 1, 8)
-
-circle_2 = Circle(8)
-print(circle_2)
-
-print(circle_2.get_area())
-
-shapes_list = [square_1, square_2, triangle_1, triangle_2, rectangle_1, circle_2]
-
-print(f"Фигура с максимальной площадью из заданного списка фигур = {get_shape_with_max_area(shapes_list)})")
+print(f"Фигура с максимальной площадью из заданного списка фигур = {get_shape_with_max_area(shapes_list)}")
 print(
-    f"Фигура со вторым по величине периметром из заданного списка фигур = {get_shape_with_next_perimeter_after_max(shapes_list)})")
+    f"Фигура со вторым по величине периметром из заданного списка фигур = {get_shape_with_next_perimeter_after_max(shapes_list)}")
+
+print(f"Фигура с максимальной площадью из заданного списка фигур = {get_shape_with_max_area(shapes_list_2)}")
+print(
+    f"Фигура со вторым по величине периметром из заданного списка фигур = {get_shape_with_next_perimeter_after_max(shapes_list_2)}")
--- shape_task/triangle.py
+++ shape_task/triangle.py
@@ -7,13 +7,12 @@ def get_side(*args):
 
 
 class Triangle(Shape):
-
     def __init__(self, x_1, y_1, x_2, y_2, x_3, y_3):
-        arg_list = [x_1, y_1, x_2, y_2, x_3, y_3]
-        for arg in range(len(arg_list)):
-            if not isinstance(arg_list[arg], (float, int)):
-                raise TypeError(f'Координаты треугольника должны быть числом, a не {type(arg_list[arg]).__name__}')
-            if arg_list[arg] < 0:
+        args_list = [x_1, y_1, x_2, y_2, x_3, y_3]
+        for arg in range(len(args_list)):
+            if not isinstance(args_list[arg], (float, int)):
+                raise TypeError(f'Координаты треугольника должны быть числом, a не {type(args_list[arg]).__name__}')
+            if args_list[arg] < 0:
                 raise ValueError('Координаты треугольника должны быть быть больше либо равны нулю')
 
         self.__x_1 = x_1
@@ -108,8 +107,8 @@ class Triangle(Shape):
         if not isinstance(other, type(self)):
             return NotImplemented
 
-        return self.__x_1 == other.__x_1 and self.__y_1 == other.__y_1 and self.__x_2 == other.__x_2 and self.__y_2 == \
-            other.__y_2 and self.__x_3 == other.__x_3 and self.__y_3 == other.__y_3
+        return self.__x_1 == other.__x_1 and self.__y_1 == other.__y_1 and self.__x_2 == other.__x_2 \
+            and self.__y_2 == other.__y_2 and self.__x_3 == other.__x_3 and self.__y_3 == other.__y_3
 
     def __hash__(self):
         return hash((self.__x_1, self.__y_1, self.__x_2, self.__y_2, self.__x_3, self.__y_3))
@@ -128,7 +127,6 @@ class Triangle(Shape):
 
     def get_area(self):
         epsilon = 1.0e-10
-
         if abs((self.__x_3 - self.__x_1) * (self.__y_2 - self.__y_1) - (self.__y_3 - self.__y_1) * (
                 self.__x_2 - self.__x_1)) <= epsilon:
             return 0
